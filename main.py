6b9381329311d9e23e4d54027747936ec640f8c2
Savannah <60404236+sxfigu19@users.noreply.github.com>
2020-01-29 14:01:58 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@
 from tkinter import * # import Tkinter library
 window = Tk()         # Create the application window
 
-# Add a label with the text "Hello"
-lbl = Label(window, text="I am Savannah")
+# Add a label with the text "Hello, I am Savvy"
+lbl = Label(window, text="Hello, I am Savvy")
 
 # Place the label in the window at 0, 0
 lbl.grid(column=0, row=0)
